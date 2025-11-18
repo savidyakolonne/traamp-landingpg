7fb0dee7a49e681d91ac3e94768be61c120a70b3
savidyakolonne <savidya.kolonne@gmail.com>
2025-11-18 10:59:37 +0530

    new update Nov18

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import HeroSection from "./components/HeroSection";
+import Partners from "./components/Partners";
 import WhySection from "./components/WhySection";
 
 
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <HeroSection />
       <WhySection/>
+      <Partners/>
    
      
     </div>
